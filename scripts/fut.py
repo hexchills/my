c1deda9ae2923ec48b04873f5dcb934f00702864
Axotol <whoami@notabug.org>
2022-06-24 21:57:06 +0300

    Initial commit

--- scripts/fut.py
+++ scripts/fut.py
@@ -3,20 +3,38 @@
 import sys
 import pandas as pd
 
-POS = float(sys.argv[1])
-SIDE = sys.argv[2]
+def test():
+    POS = float(sys.argv[3])
+    SIDE = sys.argv[1]
+    SIZE = float(sys.argv[2])
 
-if SIDE == "-l":
-    long = ((POS * 0.19) / 100 + POS)
-    long1 = str(long)
-    long2 = long1[0:7]
-    print(long2)
-    df=pd.DataFrame([long2])
-    df.to_clipboard(index=False,header=False)
-elif SIDE == "-s":
-    short = ((POS * 0.19) / 100 - POS)
-    short1 = str(short)
-    short2 = short1[1:8]
-    print(short2)
-    df1=pd.DataFrame([short2])
-    df1.to_clipboard(index=False,header=False)
+    if SIDE == "-l":
+        long = ((POS * SIZE) / 100 + POS)
+        long1 = str(long)
+        long2 = long1[0:7]
+        print(long2)
+        df=pd.DataFrame([long2])
+        df.to_clipboard(index=False,header=False)
+    
+    elif SIDE == "-s":
+        short = ((POS * SIZE) / 100 - POS)
+        short1 = str(short)
+        short2 = short1[1:8]
+        print(short2)
+        df1=pd.DataFrame([short2])
+        df1.to_clipboard(index=False,header=False)
+
+try:
+    test()
+except IndexError:
+   print("Первый аргумент: (Сторона позиции) \n\tПример: -l (long) / -s (short)")
+   print()
+   print("Второй аргумент: (Процент от цены входа): \n\tПример: 0.19 (Процент который нужно добавить к цене входа, если заходим в long позицию ИЛИ отнимаем 0.19% от цены входа, если заходим в -s (short)) ")
+   print()
+   print("Третий аргумент: (Цена входа) \n\tПример: 1001 (цена в USD / ETH)")
+   print("---" * 15)
+   print("Пример работы скрипта:")
+   print("fut -l 0.20 1100 (заходим в лонг, добавляем к цене входа 0.20%, цена входа: 1100)")
+   print("Итого: 1102.2")
+   print("fut -s 0.20 1100 (всё тоже самое, только отнимаем от цены входа 0.20%)")
+   print("Итого: 1097.8")
