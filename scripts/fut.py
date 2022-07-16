b3946098804d27b8c6f9caf3ebb0e3b00ac347a0
Axotol <whoami@notabug.org>
2022-07-16 09:23:18 +0300

    first commit

--- scripts/fut.py
+++ scripts/fut.py
@@ -1,27 +1,37 @@
 #!/bin/python3
 
 import pandas as pd
+from colorama import Fore, Back, Style
 
-print("side. (long - l / short - s)")
-SIDE = input("> ")
+print("[" + Fore.YELLOW + "?" + Fore.RESET + "] " +
+      "Side. " + Fore.CYAN + "(long - l / short - s)" + Fore.RESET)
+SIDE = input(Fore.LIGHTRED_EX + ">> " + Fore.RESET)
 
-print("---" * 9)
+print(Fore.RED + "---" * 9 + Fore.RESET)
+print()
+print("[" + Fore.YELLOW + "?" + Fore.RESET + "] " +
+      "Percent. " + Fore.CYAN + "(default - 0.159)" + Fore.RESET)
+SIZE = float(input(Fore.LIGHTRED_EX + ">> " + Fore.RESET) or "0.159")
 
-print("percent. (default - 0.159)")
-SIZE = float(input("> ") or "0.159")
+print(Fore.RED + "---" * 9 + Fore.RESET)
+print()
+print("[" + Fore.YELLOW + "?" + Fore.RESET + "] " +
+      "Stop-Loss. " + Fore.CYAN + "(default - 0.85)" + Fore.RESET)
+PERC = float(input(Fore.LIGHTRED_EX + ">> " + Fore.RESET) or "0.85")
 
-print("---" * 9)
+print(Fore.RED + "---" * 9 + Fore.RESET)
+print()
+print("[" + Fore.YELLOW + "?" + Fore.RESET + "] " +
+      "Price. " + Fore.CYAN + "(example: 1011 - ETH/USD)" + Fore.RESET)
+POS = float(input(Fore.LIGHTRED_EX + ">> " + Fore.RESET).replace(",", ""))
+print(Fore.RED + "---" * 9 + Fore.RESET)
 
-print("price. (example: 1011) ETH/USD")
-POS = float(input("> ").replace(",", ""))
-
-PERC = 0.85
 
 if SIDE == "l":
     long = ((POS * SIZE) / 100 + POS)
     long1 = str(long)
     long2 = long1[0:7]
-    print("\nLimit Order:")
+    print(Fore.YELLOW + "\nLimit Order:" + Fore.RESET)
     print(long2)
     df = pd.DataFrame([long2])
     df.to_clipboard(index=False, header=False)
@@ -30,14 +40,14 @@ if SIDE == "l":
 
     tt0 = str(stoploss)
 
-    print("\nStop Loss:")
+    print(Fore.YELLOW + "\nStop Loss:" + Fore.RESET)
     print(tt0[1:7])
 
-elif SIDE == "s":
+if SIDE == "s":
     short = ((POS * SIZE) / 100 - POS)
     short1 = str(short)
     short2 = short1[1:8]
-    print("\nLimit Order:")
+    print(Fore.YELLOW + "\nLimit Order:" + Fore.RESET)
     print(short2)
     df1 = pd.DataFrame([short2])
     df1.to_clipboard(index=False, header=False)
@@ -47,5 +57,5 @@ elif SIDE == "s":
 
     tt1 = str(stoploss1)
 
-    print("\nStop Loss:")
+    print(Fore.YELLOW + "\nStop Loss:" + Fore.RESET)
     print(tt1[0:7])
